6fd03da21f5e0a3360405c10c397bc1424a74dab
Joshua Kaplan <yhkaplan@gmail.com>
2017-11-27 13:21:50 +0900

    Restored Core Data code in AppDelegate for now so application can compile

--- Podfile
+++ Podfile
@@ -3,7 +3,9 @@ target 'StarWarsLexicon' do
     # Use frameworks instead of static libraries for Pods
     use_frameworks!
     
-    # Adding in just Realm for now
+    pod 'RxSwift', '4.0.0'
+    # To integrate RxCocoa later
+    # pod 'RxCocoa', '~> 4.0'
     pod 'RealmSwift', '3.0.2'
     # To add RxRealm later ('RxRealm')
 
@@ -13,6 +15,10 @@ end
 target 'StarWarsLexiconTests' do
     use_frameworks!
     platform :ios, '11.0'
+    pod 'RxSwift', '4.0.0'
+    # pod 'RxNimble'??
+    # pod 'RxBlocking', '~> 4.0'
+    # pod 'RxTest'??
     pod 'RealmSwift', '3.0.2'
     pod 'Nimble', '7.0.2'
     pod 'Quick', '1.2.0'
--- StarWarsLexicon/AppDelegate.swift
+++ StarWarsLexicon/AppDelegate.swift
@@ -7,12 +7,35 @@
 //
 
 import UIKit
+import CoreData
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    lazy var persistentContainer: NSPersistentContainer = {
+        let container = NSPersistentContainer(name: "StarWarsLexicon")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error: \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+    
+    func saveContext() {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let error = error as NSError
+                fatalError("\(error)")
+            }
+        }
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //This sets the status bar menu at the top to white so it's visible
         UIApplication.shared.statusBarStyle = .lightContent
